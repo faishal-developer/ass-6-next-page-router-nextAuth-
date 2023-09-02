c499c97cc982473c2e9a8e6c8d262c1e8e31e8a7
faishal-developer <foysal.professional@gmail.com>
2023-09-02 20:45:23 +0600

    nochange

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,10 +9,6 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
     // ...add more providers here
   ],
   pages: {
